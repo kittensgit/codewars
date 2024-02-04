7bd62b1cd5873bbe7a1a3a69b0f971aae37d5602
nika <nikaonime@gmail.com>
2024-02-04 13:15:58 +0200

    refactor highAndLow(split, Math.min/max)

--- 7_kyu/highest_and_lowest.js
+++ 7_kyu/highest_and_lowest.js
@@ -1,10 +1,6 @@
 const highAndLow = (numbers) => {
-    let arr = [];
-    if (numbers) {
-        numbers.split(' ').reduce((_, currVal) => arr.push(+currVal), 0);
-    }
-    arr.sort((a, b) => a - b);
-    return `${arr[arr.length - 1]} ${arr[0]}`;
+    let numbersArr = numbers.split(' ');
+    return `${Math.max(...numbersArr)} ${Math.min(...numbersArr)}`;
 };
 
 console.log(highAndLow('8 3 -5 42 -1 0 0 -9 4 7 4 -4'));
