4fb800901a03f8298050a76b01d499590c02c1a1
nika <nikaonime@gmail.com>
2024-02-07 12:59:12 +0200

    refactoring filter_list(filter, typeof)

--- 7_kyu/filter_list.js
+++ 7_kyu/filter_list.js
@@ -1,3 +1,3 @@
-const filter_list = (arr) => arr.filter((item) => Number.isInteger(item));
+const filter_list = (arr) => arr.filter((item) => typeof item == 'number');
 
 console.log(filter_list([1, 2, 'aasf', '1', '123', 123]));
