9c96192ecffb8455797219df58e648a86aaec307
nika <nikaonime@gmail.com>
2024-02-07 15:43:13 +0200

    toJadenCase(string prototype...)

--- 7_kyu/to_jaden_case.js
+++ 7_kyu/to_jaden_case.js
@@ -1,7 +1,10 @@
-const toJadenCase = (str) =>
-    str
-        .split(' ')
+String.prototype.toJadenCase = function () {
+    return this.split(' ')
         .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
         .join(' ');
+};
+
+const originalString = "How can mirrors be real if our eyes aren't real";
+const jadenCasedString = originalString.toJadenCase();
 
 console.log(toJadenCase("How can mirrors be real if our eyes aren't real"));
