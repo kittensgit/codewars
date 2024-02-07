bc7e406a4bb78c90d2bfb21221c54e3013950690
nika <nikaonime@gmail.com>
2024-02-07 22:23:31 +0200

    refactoring findOutlier(filter)

--- 6_kyu/find_outlier.js
+++ 6_kyu/find_outlier.js
@@ -1,12 +1,7 @@
 const findOutlier = (integers) => {
-    const even = [];
-    const odd = [];
-    integers.filter((el) => (el % 2 === 0 ? even.push(el) : odd.push(el)));
-    if (even.length > odd.length) {
-        return +odd.join('');
-    } else {
-        return +even.join('');
-    }
+    const even = integers.filter((el) => el % 2 === 0);
+    const odd = integers.filter((el) => el % 2 !== 0);
+    return even.length === 1 ? even[0] : odd[0];
 };
 
 console.log(findOutlier([1, 1, 0, 1, 1]));
