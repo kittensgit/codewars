71f69bc5af46d787579bf24b706178db85a9108c
nika <nikaonime@gmail.com>
2024-02-07 20:47:59 +0200

    clever solution for createPhoneNumber(replace)

--- 6_kyu/create_phone_number.js
+++ 6_kyu/create_phone_number.js
@@ -1,6 +1,4 @@
 const createPhoneNumber = (numbers) =>
-    `(${numbers.slice(0, 3).join('')}) ${numbers.slice(3, 6).join('')}-${numbers
-        .slice(6, 10)
-        .join('')}`;
+    numbers.reduce((acc, curr) => acc.replace('x', curr), '(xxx) xxx-xxxx');
 
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
